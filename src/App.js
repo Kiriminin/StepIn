17a3a5f4258219edfd0442c20992f9ed5ee6fa8e
Kiriminin <fenix139931@gmail.com>
2025-08-05 00:16:59 +0300

    Fix merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import React from 'react';
-<<<<<<< HEAD
-import LandingHeader from './components/LandingHeader';
-import WelcomeSection from './components/WelcomeSection';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <LandingHeader />
-      <WelcomeSection />
-    </div>
-=======
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';    
 import MainPage from './pages/MainScreen'; 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import Footer from './components/common/Footer/Footer';
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/home" element={<MainPage />} />
-      </Routes> 
-    </BrowserRouter>  
->>>>>>> 73d59c9 (Commit)
+      </Routes>  
   );
 }
 
